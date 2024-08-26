705c96d0bf242730b5c461d86dd28905d4db68e1
“satyaprakash-yadav” <satyay9798@gmail.com>
2024-08-26 20:57:55 +0530

    feat: Landing Page Added

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,13 @@
+type Props = {
+  children: React.ReactNode;
+};
+
+const Layout = ({ children }: Props) => {
+  return (
+    <div className="flex overflow-hidden h-screen">
+      <div className="w-full">{children}</div>
+    </div>
+  );
+};
+
+export default Layout;
--- components/global/navbar.tsx
+++ components/global/navbar.tsx
@@ -3,9 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-type Props = {};
-
-const Navbar = (props: Props) => {
+const Navbar = () => {
   return (
     <header className="fixed right-0 left-0 top-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between">
       <aside className="flex items-center gap-[2px]">
